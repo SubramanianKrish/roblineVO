bddf9768157733f412e019ec05aa576cb28637ae
awadhutthube <awadhutthube@gmail.com>
2020-11-09 16:13:49 -0500

    Added line descriptor and matching

--- src/eigen_test.cpp
+++ src/eigen_test.cpp
@@ -20,14 +20,17 @@ int main(int argc, char* argv[])
 
     vo_line line_obj;
     string data_dir     = "../data/lines.png";
+
     cv::Mat rgb_img   = ReadImage(data_dir);
     cv::Mat edge_image = line_obj.GetEdgeImage(rgb_img, 50, 150, 3, true);
     
     Eigen::MatrixXi linesP; // will hold the results of the detection
     linesP = line_obj.GetHoughLinesP(edge_image, 50, 50, 10);
-    // std::cout << linesP.transpose()*linesP << std::endl;
+
     cv::Mat img_with_lines = line_obj.DrawHoughLinesP(rgb_img, linesP);
     std::vector<std::vector<cv::Point2i>> sampled_lines = line_obj.SampleIndices(linesP, 480, 640);
+
     DisplayImage(img_with_lines);
+    
     return 0;
 }
---
+++ src/lsd_test.cpp
@@ -0,0 +1,14 @@
+#include "LineMatchingAlgorithm.hh"
+#include "LineStructure.hh"
+
+using namespace std;
+
+typedef struct PairData pstruct; 
+
+int main(int argc, char **argv)
+{
+	std::cout << "first checkpt" << std::endl;
+	pstruct result;
+    result = image_process("../data/pic1.png","../data/pic2.png","../data/out.png","",false);
+    return 0;
+}
