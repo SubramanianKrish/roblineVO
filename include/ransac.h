31073c582a4a01aa60644f662acd5358c96b3d36
Subramanian Krishnan <subramanian.krishnan@vecnarobotics.com>
2020-11-28 18:51:14 -0500

    added single line ransac. tuning tbd

--- include/ransac.h
+++ include/ransac.h
@@ -16,7 +16,20 @@ typedef Eigen::Vector3d point3d;
 typedef Eigen::Matrix3d covariance;
 
 class Ransac{
-
 public:
+    
+    int n_iterations;
+    double threshold;
+
+    std::random_device rd;   // hardware generated random number
+    std::mt19937 generator;  // seeding
+
+    Ransac(int n_iterations, double threshold);
+
+    // Returns the mahalanobis distance between a point and a line
     double mahalanobis_distance(const point3d& X, const covariance& sigma_x, const point3d& A, const point3d& B);
+
+    // Returns a set of 3D points that are considered inliers to the estimated line
+    Eigen::Matrix3Xd removeOutlierPoints(const Eigen::Matrix3Xd& linepoints, const std::vector<Eigen::Matrix3d>& cov);
+
 };
--- src/ransac.cpp
+++ src/ransac.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
+#include <random>
 #include <Eigen/Geometry>
 
 #include "ransac.h"
 
 using namespace std;
+
+Ransac::Ransac(int n_iterations, double threshold): n_iterations(n_iterations),
+                                                    threshold(threshold)
+{    // Seed the generator
+    generator.seed(rd());
+};
 
 double Ransac::mahalanobis_distance(const point3d& X, const covariance& sigma_x, const point3d& A, const point3d& B){
     Eigen::SelfAdjointEigenSolver<covariance> eigensolver(sigma_x);
@@ -24,4 +31,35 @@ double Ransac::mahalanobis_distance(const point3d& X, const covariance& sigma_x,
     double distance = A_affine.cross(B_affine).norm()/(A_affine - B_affine).norm();
 
     return distance;
+}
+
+Eigen::Matrix3Xd Ransac::removeOutlierPoints(const Eigen::Matrix3Xd& linepoints, const std::vector<Eigen::Matrix3d>& cov){
+    Eigen::Matrix3Xd best_inliers;
+    int best_num_inliers = -1;
+    float distance;
+
+    // Setup the range from which we choose random numbers
+    std::uniform_int_distribution<unsigned> distribution(0,linepoints.cols()-1); // end inclusive
+
+    for(int iter=0; iter < n_iterations; ++iter){
+        int inliers = 0;
+        int i = distribution(generator), j = distribution(generator);
+        Eigen::Matrix3Xd support_points;
+        Eigen::Vector3d A = linepoints.col(i), B = linepoints.col(j);
+
+        // calculate support for chosen endpoint indices
+        for(int k=0; k < linepoints.cols(); ++k){
+            if(k==i or k==j) continue;
+            distance = mahalanobis_distance(linepoints.col(k), cov[k], A, B);
+            cout << distance << endl;
+            if (distance < threshold) support_points.col(inliers++) = linepoints.col(k);
+        }
+
+        if(inliers > best_num_inliers){
+            best_num_inliers = inliers;
+            best_inliers = support_points.leftCols(inliers);
+        }
+    }
+    
+    return best_inliers;
 }
